d97fa5182ebfaa114be789af4d3501d70c6aeac2
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2019-07-13 11:30:12 -0700

    fix (.Net) use .Net 4.7.2 instead of .Net 4.5.2

--- src/Application/Properties/AssemblyInfo.cs
+++ src/Application/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f1214dc8-624e-4869-94ed-62fda24b6820")]
+[assembly: Guid("93ba5bd4-cdac-4d60-9149-32d892b49005")]
 
 // Version information for an assembly consists of the following four values:
 //
--- src/Website/Properties/AssemblyInfo.cs
+++ src/Website/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a74b93bd-3a15-4e75-9cac-2c3fc948f7d1")]
+[assembly: Guid("95daf328-e001-4a8d-be71-73c3dec146d7")]
 
 // Version information for an assembly consists of the following four values:
 //
